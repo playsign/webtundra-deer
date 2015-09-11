23a9afb5ab0e2735c40920a51395f42f7fc5addd
Toni Alatalo <toni@playsign.net>
2015-09-11 11:14:09 +0300

    webtundra2 load deer. starting point: no materials nor skinning. the library should IMHO load and apply those automatically so hopefully an update to the lib makes this nice later.

---
+++ loaddeer.js
@@ -0,0 +1,194 @@
+var DeerApplication = ICameraApplication.$extend(
+{
+    __init__ : function()
+    {
+        this.$super("Deer");
+
+        // Enables animations if both the previous and this camera entity is unparented
+        this.animateBeforeActivation(true);
+        
+        this.showTooltip = TundraSDK.framework.frame.wallClockTime() + 3;
+        this.tooltipVisible = false;
+        
+        this.demoApp = {
+            origin: new THREE.Vector3(0, -10, -30),
+            world : {},
+            
+            createScene : function()
+            {
+                this.world["Ground"] = this._createMesh("Ground", "webtundra://plane.json");
+                this.world["Ground"].name = "Ground";
+                t = this.world["Ground"].placeable.transform;
+                t.setPosition(this.origin.x, this.origin.y, this.origin.z);
+                t.setScale(100, 2, 100);
+                this.world["Ground"].placeable.transform = t;
+                
+                var dirLight = new THREE.DirectionalLight();
+                dirLight.intensity = 0.5;
+                TundraSDK.framework.client.renderer.scene.add(dirLight);
+
+		var deer = this._createMesh("Deer", "webtundra://White_Tailed_Deer_all.json");
+		
+            },
+            
+            _createMesh : function(name, ref)
+            {
+                var meshEntity = TundraSDK.framework.scene.createLocalEntity(["Name", "Placeable", "Mesh"]);
+                meshEntity.name = name;
+                meshEntity.mesh.meshRef = ref;
+                
+                return meshEntity;
+            }
+        };
+        
+        this.createCamera();
+	this.demoApp.createScene();
+    },
+
+    createCamera : function()
+    {
+        this.onDisconnected();
+
+        this.movement = new THREE.Vector3(0,0,0);
+        this.movementForce = 10.0;
+
+        // Connect input
+        this.subscribeEvent(TundraSDK.framework.input.onMouseMove(this, this.onMouseMove));
+        this.subscribeEvent(TundraSDK.framework.input.onKeyEvent(this, this.onKeyEvent));
+
+        // Connect frame update
+        this.subscribeEvent(TundraSDK.framework.frame.onUpdate(this, this.onUpdate));
+
+        // Start up the ICameraApplication
+        this.startCameraApplication("Free Look", "FreeLookCamera", 60);
+
+        // Initiate camera pos/rot and activate it
+        var t = this.cameraEntity.placeable.transform;
+        t.pos.y = 2;
+        t.pos.z = 3;
+        this.cameraEntity.placeable.transform = t;
+        this.cameraEntity.camera.setActive();
+    },
+
+    onDisconnected : function()
+    {
+        this.resetCameraApplication();
+        this.unsubscribeEvents();
+    },
+
+    onCameraActived : function(cameraEntity, previousCameraEntity)
+    {
+        this.movement.x = 0; this.movement.y = 0; this.movement.z = 0;
+    },
+
+    onCameraDeactived : function(cameraEntity, activatedCameraEntity)
+    {
+        this.movement.x = 0; this.movement.y = 0; this.movement.z = 0;
+    },
+
+    onUpdate : function(frametime)
+    {
+        if (this.cameraEntity.camera === null || !this.cameraEntity.camera.active)
+            return;
+
+        if (this.movement.x != 0 || this.movement.y != 0 || this.movement.z != 0)
+        {
+            var t = this.cameraEntity.placeable.transform;
+            var relativeMovement = this.movement.clone();
+            relativeMovement.applyQuaternion(t.orientation());
+            relativeMovement.multiplyScalar(frametime * this.movementForce)
+            t.pos.x += relativeMovement.x;
+            t.pos.y += relativeMovement.y;
+            t.pos.z += relativeMovement.z;
+            this.cameraEntity.placeable.transform = t;
+        }
+        
+        if (TundraSDK.framework.frame.wallClockTime() >= this.showTooltip &&
+            !this.tooltipVisible)
+        {
+            this.tooltipVisible = true;
+            ICameraApplication.showCameraApplicationInfoTooltip("", 30000);
+            ICameraApplication.cameraApplicationTooltip.text("R = Reset E = Spawn Projectile");
+        }
+    },
+
+    onKeyEvent : function(event)
+    {
+        if (!this.cameraEntity.camera.active)
+            return;
+
+        if (event.type === "release")
+        {
+            if (event.key == "w" || event.key == "up" || event.key == "s" ||  event.key == "down")
+                this.movement.z = 0;
+            else if (event.key == "a" || event.key == "d" || event.key == "left" || event.key == "right")
+                this.movement.x = 0;
+            else if (event.key == "c" || event.key == "space")
+                this.movement.y = 0;
+            else if (event.key == "r")
+            {
+                this.demoApp.release();
+            }
+        }
+
+        if (event.targetNodeName !== "body")
+        {
+            this.movement.x = 0; this.movement.y = 0; this.movement.z = 0;
+            return;
+        }
+
+        var speed = 1;
+        if (event.pressed["shift"])
+            speed *= 2;
+
+        this.movement.x = 0; this.movement.y = 0; this.movement.z = 0;
+        for (var key in event.pressed)
+        {
+            if (key == "w" || key == "up")
+                this.movement.z = -speed;
+            else if (key == "s" || key == "down")
+                this.movement.z = speed;
+            else if (key == "a" || key == "left")
+                this.movement.x = -speed;
+            else if (key == "d" || key == "right")
+                this.movement.x = speed;
+            else if (key == "c")
+                this.movement.y = -speed;
+            else if (key == "space")
+                this.movement.y = speed;
+        }
+    },
+
+    onMouseMove : function(event)
+    {
+        if (!this.cameraEntity.camera.active)
+            return;
+
+        if (!event.rightDown)
+            return;
+        if (event.targetNodeName !== "canvas")
+            return;
+        if (event.relativeX == 0 && event.relativeY == 0)
+            return;
+
+        if (event.relativeX != 0 || event.relativeY != 0)
+        {
+            var t = this.cameraEntity.placeable.transform;
+            if (event.relativeX != 0)
+            {
+                t.rot.y -= (event.relativeX * 0.2);
+                if (t.rot.y > 360.0 || t.rot.y < -360.0)
+                    t.rot.y = t.rot.y % 360.0;
+            }
+            if (event.relativeY != 0)
+            {
+                t.rot.x -= (event.relativeY * 0.2);
+                if (t.rot.x > 90.0)
+                    t.rot.x = 90.0;
+                else if (t.rot.x < -90.0)
+                    t.rot.x = -90.0;
+            }
+            this.cameraEntity.placeable.transform = t;
+        }
+    }
+});
